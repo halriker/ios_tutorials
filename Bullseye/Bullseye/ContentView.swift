86bb78994d1d493d14a0fdfd4fca061f0b945938
Hal Riker <halriker@gmail.com>
2021-01-19 18:06:12 -0500

    Import Swift UI Book Code
    https://github.com/raywenderlich/sui-materials

--- Bullseye/Bullseye/ContentView.swift
+++ Bullseye/Bullseye/ContentView.swift
@@ -7,9 +7,14 @@
 
 import SwiftUI
 
+// This is a structure named ContentView and it's a view.
+// A view is an object in SwiftUI and it represents anything on the screen. Views can contain other views.
+// Any properties and methods defined inside ContentView is in addition to what it gets from the view.
 struct ContentView: View {
+    // The body (object) is a property. It acts as a container for all the objects on the screen.
+    // var = 1. It's a container for data 2. It's content can change. 
     var body: some View {
-        Text("Hello, world!")
+        Text("My Name is Hal")
             .padding()
     }
 }
